ff199e169571e5ad8d9fcb53cea36767f9ceb25d
Kenneth Rawlings <rawlink@users.noreply.github.com>
2016-04-30 16:53:02 -0600

    Feature/authorization (#4)
    * Initial attempt at authorization. Doesn't work. Checking in before refactoring packages and imports.
    
    * Refactor directory structure. Fix auth algo bug.
    
    * Ran gofmt to clean up code formatting
    
    * Add missing required admin check.

---
+++ middleware/auth.go
@@ -0,0 +1,39 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/infiniteprimates/smoke/util"
+)
+
+func AuthorizationMiddleware(requireAdmin bool) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token, err := jwt.ParseFromRequest(ctx.Request, keyFunc)
+
+		if err != nil || !token.Valid {
+			util.AbortWithStatus(ctx, http.StatusForbidden)
+			return
+		}
+
+		if requireAdmin && !token.Claims["isAdmin"].(bool) {
+			util.AbortWithStatus(ctx, http.StatusForbidden)
+			return
+		}
+
+		ctx.Set("username", token.Claims["sub"])
+		ctx.Set("isAdmin", token.Claims["isAdmin"])
+
+		ctx.Next()
+	}
+}
+
+func keyFunc(token *jwt.Token) (interface{}, error) {
+	if method, ok := token.Method.(*jwt.SigningMethodHMAC); !ok || method.Name != "HS256" {
+		return nil, fmt.Errorf("Unexpected signing method %v", token.Header["alg"])
+	}
+
+	return []byte("s3kr1t"), nil
+}
--- user/user.go
+++
@@ -1,7 +0,0 @@
-package user
-
-type User struct {
-	Username string
-	Password string
-	IsAdmin bool
-}
