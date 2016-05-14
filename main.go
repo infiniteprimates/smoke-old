94ed8c72b0998184a5570f7f608d9230ee017ec3
Kenneth Rawlings <rawlink@gmail.com>
2016-05-13 23:33:34 -0600

    Start refactor to make testing easier.

--- main.go
+++ main.go
@@ -1,48 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"log"
-	"net/http"
 	"time"
 
 	"github.com/Sirupsen/logrus"
-	"github.com/gin-gonic/gin"
 	"github.com/infiniteprimates/smoke/config"
-	"github.com/infiniteprimates/smoke/rest"
+	"github.com/infiniteprimates/smoke/server"
 	"github.com/rcrowley/go-metrics"
 	"github.com/spf13/viper"
+	"github.com/infiniteprimates/smoke/db"
 )
 
 func main() {
-	config.Init()
-	startServer()
-}
-
-func startServer() {
 	logger := logrus.New()
 	logWriter := logger.Writer()
 	defer logWriter.Close()
 
+	config.Init()
+
+	startMetricsLogging(logWriter)
+
+	db := db.New()
+
+	server := server.New(logWriter, db)
+	server.Start()
+}
+
+func startMetricsLogging(logWriter io.Writer) {
 	// Start background metrics logger
 	metricsLoggingInterval := time.Duration(viper.GetInt(config.METRICS_LOGGING_INTERVAL)) * time.Minute
 	go metrics.Log(metrics.DefaultRegistry, metricsLoggingInterval, log.New(logWriter, "metrics", log.Lmicroseconds))
-
-	router := gin.New()
-	router.Use(gin.LoggerWithWriter(logWriter))
-	router.Use(gin.RecoveryWithWriter(logWriter))
-
-	//TODO: Figure out how to do static better with router.NoRoute and contrib static. Doesn't work right though.
-	router.Any("/", func(ctx *gin.Context) { ctx.Redirect(http.StatusTemporaryRedirect, "/ui/") })
-	router.Static("/ui", "ui")
-
-	createResources(router)
-
-	ipAndPort := fmt.Sprintf("%s:%d", viper.GetString(config.IP), viper.GetInt(config.PORT))
-	router.Run(ipAndPort)
-}
-
-func createResources(router gin.IRouter) {
-	rest.CreateAuthResources(router)
-	rest.CreateUserResources(router)
 }
