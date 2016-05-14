addfc8b6f2db01e5a9d0cb44301ce6bbb9991faa
Chris Rawlings <rawlinc@gmail.com>
2016-05-14 14:33:35 -0600

    Add the initial angular 2 UI skeleton (#8)

---
+++ ui/src/app/smoke.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'smoke-app',
+  templateUrl: 'smoke.component.html',
+  styleUrls: ['smoke.component.css']
+})
+export class SmokeAppComponent {
+  title = 'smoke works!';
+}
