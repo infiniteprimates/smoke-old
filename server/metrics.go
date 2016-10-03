965f782694cdd9a410dde1d24919a7a9aa56b8f1
Kenneth Rawlings <rawlink@users.noreply.github.com>
2016-10-02 22:18:22 -0600

    Feature/metrics tests (#30)
    * Remove unneeded counter metrics.
    
    * Add metrics test file.
    
    * Add some metric test scenarios.
    
    * Add new test. Refactor metric names and handling.
    
    * Finish metric tests.

--- server/metrics.go
+++ server/metrics.go
@@ -15,7 +15,7 @@ import (
 
 func startMetricsServer(cfg config.Config, log log.Logger) {
 	// start publishing metrics into expvars
-	go metricsToExpvar(time.Duration(cfg.GetInt(config.MetricsPublishingInterval)) * time.Second)
+	go gatherMetrics(time.Duration(cfg.GetInt(config.MetricsPublishingInterval)) * time.Second)
 
 	go func() {
 		ipAndPort := fmt.Sprintf("%s:%d", cfg.GetString(config.MetricsIp), cfg.GetInt(config.MetricsPort))
@@ -24,73 +24,87 @@ func startMetricsServer(cfg config.Config, log log.Logger) {
 	}()
 }
 
-func metricsToExpvar(interval time.Duration) {
+func gatherMetrics(interval time.Duration) {
 	smokeMetrics := expvar.NewMap("smokeMetrics")
 
 	for _ = range time.Tick(interval) {
-		metrics.DefaultRegistry.Each(func(name string, i interface{}) {
-			switch metric := i.(type) {
-			case metrics.Counter:
-				smokeMetrics.Add(name, metric.Count())
-			case metrics.Gauge:
-				smokeMetrics.Add(name, metric.Value())
-			case metrics.GaugeFloat64:
-				smokeMetrics.AddFloat(name, metric.Value())
-			case metrics.Histogram:
-				h := metric.Snapshot()
-				ps := h.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
-				smokeMetrics.Add(name+".count", h.Count())
-				smokeMetrics.AddFloat(name+".min", float64(metric.Min()))
-				smokeMetrics.AddFloat(name+".min", float64(h.Min()))
-				smokeMetrics.AddFloat(name+".max", float64(h.Max()))
-				smokeMetrics.AddFloat(name+".mean", float64(h.Mean()))
-				smokeMetrics.AddFloat(name+".std-dev", float64(h.StdDev()))
-				smokeMetrics.AddFloat(name+".50-percentile", float64(ps[0]))
-				smokeMetrics.AddFloat(name+".75-percentile", float64(ps[1]))
-				smokeMetrics.AddFloat(name+".95-percentile", float64(ps[2]))
-				smokeMetrics.AddFloat(name+".99-percentile", float64(ps[3]))
-				smokeMetrics.AddFloat(name+".999-percentile", float64(ps[4]))
-			case metrics.Meter:
-				m := metric.Snapshot()
-				smokeMetrics.Add(name+".count", m.Count())
-				smokeMetrics.AddFloat(name+".one-minute", float64(m.Rate1()))
-				smokeMetrics.AddFloat(name+".five-minute", float64(m.Rate5()))
-				smokeMetrics.AddFloat(name+".fifteen-minute", float64((m.Rate15())))
-				smokeMetrics.AddFloat(name+".mean", float64(m.RateMean()))
-			case metrics.Timer:
-				t := metric.Snapshot()
-				ps := t.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
-				smokeMetrics.Add(name+".count", t.Count())
-				smokeMetrics.AddFloat(name+".min", float64(t.Min()))
-				smokeMetrics.AddFloat(name+".max", float64(t.Max()))
-				smokeMetrics.AddFloat(name+".mean", float64(t.Mean()))
-				smokeMetrics.AddFloat(name+".std-dev", float64(t.StdDev()))
-				smokeMetrics.AddFloat(name+".50-percentile", float64(ps[0]))
-				smokeMetrics.AddFloat(name+".75-percentile", float64(ps[1]))
-				smokeMetrics.AddFloat(name+".95-percentile", float64(ps[2]))
-				smokeMetrics.AddFloat(name+".99-percentile", float64(ps[3]))
-				smokeMetrics.AddFloat(name+".999-percentile", float64(ps[4]))
-				smokeMetrics.AddFloat(name+".one-minute", float64(t.Rate1()))
-				smokeMetrics.AddFloat(name+".five-minute", float64(t.Rate5()))
-				smokeMetrics.AddFloat(name+".fifteen-minute", float64((t.Rate15())))
-				smokeMetrics.AddFloat(name+".mean-rate", float64(t.RateMean()))
-			default:
-				panic(fmt.Sprintf("unsupported type for '%s': %T", name, i))
-			}
-		})
+		metricsToExpvar(smokeMetrics)
 	}
 }
 
+func metricsToExpvar(expvarMap *expvar.Map) {
+	metrics.DefaultRegistry.Each(func(name string, i interface{}) {
+		switch metric := i.(type) {
+		case metrics.Counter:
+			setInt(expvarMap, name, metric.Count())
+		case metrics.Gauge:
+			setInt(expvarMap, name, metric.Value())
+		case metrics.GaugeFloat64:
+			var gauge expvar.Float
+			gauge.Set(metric.Value())
+			expvarMap.Set(name, &gauge)
+		case metrics.Histogram:
+			snapshot := metric.Snapshot()
+			percentiles := snapshot.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
+			setInt(expvarMap, name+".count", snapshot.Count())
+			setInt(expvarMap, name+".min", snapshot.Min())
+			setInt(expvarMap, name+".max", snapshot.Max())
+			setFloat(expvarMap, name+".mean", snapshot.Mean())
+			setFloat(expvarMap, name+".std-dev", snapshot.StdDev())
+			setFloat(expvarMap, name+".p50", percentiles[0])
+			setFloat(expvarMap, name+".p75", percentiles[1])
+			setFloat(expvarMap, name+".p95", percentiles[2])
+			setFloat(expvarMap, name+".p99", percentiles[3])
+			setFloat(expvarMap, name+".p999", percentiles[4])
+		case metrics.Meter:
+			snapshot := metric.Snapshot()
+			setInt(expvarMap, name+".count", snapshot.Count())
+			setFloat(expvarMap, name+".1min", snapshot.Rate1())
+			setFloat(expvarMap, name+".5min", snapshot.Rate5())
+			setFloat(expvarMap, name+".15min", snapshot.Rate15())
+			setFloat(expvarMap, name+".mean-rate", snapshot.RateMean())
+		case metrics.Timer:
+			snapshot := metric.Snapshot()
+			percentiles := snapshot.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
+			setInt(expvarMap, name+".count", snapshot.Count())
+			setInt(expvarMap, name+".min", snapshot.Min())
+			setInt(expvarMap, name+".max", snapshot.Max())
+			setFloat(expvarMap, name+".mean", snapshot.Mean())
+			setFloat(expvarMap, name+".std-dev", snapshot.StdDev())
+			setFloat(expvarMap, name+".p50", percentiles[0])
+			setFloat(expvarMap, name+".p75", percentiles[1])
+			setFloat(expvarMap, name+".p95", percentiles[2])
+			setFloat(expvarMap, name+".p99", percentiles[3])
+			setFloat(expvarMap, name+".p999", percentiles[4])
+			setFloat(expvarMap, name+".1min", snapshot.Rate1())
+			setFloat(expvarMap, name+".5min", snapshot.Rate5())
+			setFloat(expvarMap, name+".15min", snapshot.Rate15())
+			setFloat(expvarMap, name+".mean-rate", snapshot.RateMean())
+		default:
+			panic(fmt.Sprintf("unsupported type for '%s': %T", name, i))
+		}
+	})
+}
+
+func setInt(expvarMap *expvar.Map, name string, value int64) {
+	var expvarInt expvar.Int
+	expvarInt.Set(value)
+	expvarMap.Set(name, &expvarInt)
+}
+
+func setFloat(expvarMap *expvar.Map, name string, value float64) {
+	var expvarFloat expvar.Float
+	expvarFloat.Set(value)
+	expvarMap.Set(name, &expvarFloat)
+}
+
 func metricsMiddleware(name string) echo.MiddlewareFunc {
-	counter := metrics.NewCounter()
 	timer := metrics.NewTimer()
 
-	metrics.Register(fmt.Sprintf("api.%s.counter", name), counter)
 	metrics.Register(fmt.Sprintf("api.%s.timer", name), timer)
 
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			counter.Inc(1)
 			start := time.Now()
 			defer timer.UpdateSince(start)
 			return next(c)
---
+++ server/metrics_test.go
@@ -0,0 +1,159 @@
+package server
+
+import (
+	"expvar"
+	"net/http"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/test"
+	"github.com/rcrowley/go-metrics"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMetrics_metricsToExpvar_Counter(t *testing.T) {
+	counter := metrics.NewCounter()
+	metrics.Register("counter", counter)
+	counter.Inc(22)
+
+	var smokeMetrics expvar.Map
+	smokeMetrics.Init()
+
+	metricsToExpvar(&smokeMetrics)
+	// a second time to make sure things aren't incorrectly accumulated
+	metricsToExpvar(&smokeMetrics)
+
+	assert.Equal(t, "22", smokeMetrics.Get("counter").String(), "Unexpected counter value.")
+}
+
+func TestMetrics_metricsToExpvar_Gauge(t *testing.T) {
+	gauge := metrics.NewGauge()
+	metrics.Register("gauge", gauge)
+	gauge.Update(220)
+
+	var smokeMetrics expvar.Map
+	smokeMetrics.Init()
+
+	metricsToExpvar(&smokeMetrics)
+	// a second time to make sure things aren't incorrectly accumulated
+	metricsToExpvar(&smokeMetrics)
+
+	assert.Equal(t, "220", smokeMetrics.Get("gauge").String(), "Unexpected gauge value.")
+}
+
+func TestMetrics_metricsToExpvar_GaugeFloat64(t *testing.T) {
+	gaugefloat64 := metrics.NewGaugeFloat64()
+	metrics.Register("gaugefloat64", gaugefloat64)
+	gaugefloat64.Update(.223)
+
+	var smokeMetrics expvar.Map
+	smokeMetrics.Init()
+
+	metricsToExpvar(&smokeMetrics)
+	// a second time to make sure things aren't incorrectly accumulated
+	metricsToExpvar(&smokeMetrics)
+
+	assert.Equal(t, "0.223", smokeMetrics.Get("gaugefloat64").String(), "Unexpected gaugefloat64 value.")
+}
+
+func TestMetrics_metricsToExpvar_Histogram(t *testing.T) {
+	sample := metrics.NewUniformSample(4)
+	histogram := metrics.NewHistogram(sample)
+	metrics.Register("histogram", histogram)
+	histogram.Update(223)
+	histogram.Update(260)
+	histogram.Update(270)
+	histogram.Update(308)
+	histogram.Update(357)
+
+	var smokeMetrics expvar.Map
+	smokeMetrics.Init()
+
+	metricsToExpvar(&smokeMetrics)
+	// a second time to make sure things aren't incorrectly accumulated
+	metricsToExpvar(&smokeMetrics)
+
+	assert.Equal(t, "5", smokeMetrics.Get("histogram.count").String(), "Unexpected histogram count value.")
+	assert.Equal(t, "260", smokeMetrics.Get("histogram.min").String(), "Unexpected histogram min value.")
+	assert.Equal(t, "357", smokeMetrics.Get("histogram.max").String(), "Unexpected histogram max value.")
+	assert.Equal(t, "298.75", smokeMetrics.Get("histogram.mean").String(), "Unexpected histogram mean value.")
+	assert.Equal(t, "38.10101704679286", smokeMetrics.Get("histogram.std-dev").String(), "Unexpected histogram std-dev value.")
+	assert.Equal(t, "289", smokeMetrics.Get("histogram.p50").String(), "Unexpected histogram p50 value.")
+	assert.Equal(t, "344.75", smokeMetrics.Get("histogram.p75").String(), "Unexpected histogram p75 value.")
+	assert.Equal(t, "357", smokeMetrics.Get("histogram.p95").String(), "Unexpected histogram p95 value.")
+	assert.Equal(t, "357", smokeMetrics.Get("histogram.p99").String(), "Unexpected histogram p99 value.")
+	assert.Equal(t, "357", smokeMetrics.Get("histogram.p999").String(), "Unexpected histogram p999 value.")
+}
+
+func TestMetrics_metricsToExpvar_Meter(t *testing.T) {
+	meter := metrics.NewMeter()
+	metrics.Register("meter", meter)
+	meter.Mark(410)
+	meter.Mark(44)
+	meter.Mark(45)
+	meter.Mark(454)
+
+	var smokeMetrics expvar.Map
+	smokeMetrics.Init()
+
+	metricsToExpvar(&smokeMetrics)
+	// a second time to make sure things aren't incorrectly accumulated
+	metricsToExpvar(&smokeMetrics)
+
+	assert.Equal(t, "953", smokeMetrics.Get("meter.count").String(), "Unexpected meter count value.")
+	assert.Equal(t, "0", smokeMetrics.Get("meter.1min").String(), "Unexpected meter 1min value.")
+	assert.Equal(t, "0", smokeMetrics.Get("meter.5min").String(), "Unexpected meter 5min value.")
+	assert.Equal(t, "0", smokeMetrics.Get("meter.15min").String(), "Unexpected meter 15min value.")
+	assert.NotZero(t, smokeMetrics.Get("meter.mean-rate").String(), "Unexpected meter 15min value.")
+}
+
+func TestMetrics_metricsToExpvar_Timer(t *testing.T) {
+	timer := metrics.NewTimer()
+	metrics.Register("timer", timer)
+
+	timer.Update(2 * time.Microsecond)
+	timer.Update(3 * time.Microsecond)
+	timer.Update(5 * time.Microsecond)
+	timer.Update(7 * time.Microsecond)
+	timer.Update(11 * time.Microsecond)
+
+	var smokeMetrics expvar.Map
+	smokeMetrics.Init()
+
+	metricsToExpvar(&smokeMetrics)
+	// a second time to make sure things aren't incorrectly accumulated
+	metricsToExpvar(&smokeMetrics)
+
+	assert.Equal(t, "5", smokeMetrics.Get("timer.count").String(), "Unexpected timer count value.")
+	assert.Equal(t, "2000", smokeMetrics.Get("timer.min").String(), "Unexpected timer min value.")
+	assert.Equal(t, "11000", smokeMetrics.Get("timer.max").String(), "Unexpected timer max value.")
+	assert.Equal(t, "5600", smokeMetrics.Get("timer.mean").String(), "Unexpected timer mean value.")
+	assert.Equal(t, "3200", smokeMetrics.Get("timer.std-dev").String(), "Unexpected timer std-dev value.")
+	assert.Equal(t, "5000", smokeMetrics.Get("timer.p50").String(), "Unexpected timer p50 value.")
+	assert.Equal(t, "9000", smokeMetrics.Get("timer.p75").String(), "Unexpected timer p75 value.")
+	assert.Equal(t, "11000", smokeMetrics.Get("timer.p95").String(), "Unexpected timer p95 value.")
+	assert.Equal(t, "11000", smokeMetrics.Get("timer.p99").String(), "Unexpected timer p99 value.")
+	assert.Equal(t, "11000", smokeMetrics.Get("timer.p999").String(), "Unexpected timer p999 value.")
+	assert.Equal(t, "0", smokeMetrics.Get("timer.1min").String(), "Unexpected timer 1min value.")
+	assert.Equal(t, "0", smokeMetrics.Get("timer.5min").String(), "Unexpected timer 5min value.")
+	assert.Equal(t, "0", smokeMetrics.Get("timer.15min").String(), "Unexpected timer 15min value.")
+	assert.NotZero(t, "578770.6910522052", smokeMetrics.Get("timer.mean-rate").String(), "Unexpected timer mean-rate value.")
+}
+
+func TestMetrics_metricsMiddleware(t *testing.T) {
+	e := echo.New()
+	req := test.NewRequest(echo.GET, "/", strings.NewReader(""))
+	res := test.NewResponseRecorder()
+	c := e.NewContext(req, res)
+
+	mw := metricsMiddleware("rest")
+	err := mw(func(ctx echo.Context) error { return c.String(http.StatusOK, "OK") })(c)
+
+	if assert.NoError(t, err, "An error occured invoking handler.") {
+		timer := metrics.DefaultRegistry.Get("api.rest.timer").(metrics.Timer)
+		assert.NotZero(t, timer.Min(), "Timer duration was zero.")
+
+	}
+}
